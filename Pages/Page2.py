89ecd4229246b5913773a98c33f8b39a2012050f
Alen <alen.veselic01@gmail.com>
2025-03-16 17:47:12 +0100

    Add ability to generate diet plan in GUI

--- Pages/Page2.py
+++ Pages/Page2.py
@@ -1,10 +1,31 @@
 from tkinter import *
+import ttkbootstrap as ttk
 
 from Pages.Page_Class import Page
+import dietPlanner
 
 
 class Page2(Page):
+    label = None
+
     def __init__(self, root, *args, **kwargs):
         Page.__init__(self, root, *args, **kwargs)
-        label = Label(self, text="This is page 2")
-        label.pack(side="top", fill="both", expand=True)
+        self.label = Label(self, text="No diet plan generated yet.")
+        self.label.pack(side="top", fill="both", expand=True)
+
+        frame = ttk.Frame(self)
+
+        createDietPlanButton = ttk.Button(
+            frame,
+            text="Create Diet Plan",
+            command=self.updateLabelWithDietPlan,
+        )
+
+        createDietPlanButton.pack()
+        frame.pack(side="top", fill="both", expand=True)
+
+    def updateLabelWithDietPlan(self):
+        self.label["text"] = "\n".join(
+            "{}\n {}\n".format(k, d)
+            for k, d in (dietPlanner.createPlan(5, 2, 2)).items()
+        ).replace(",", ",\n")
