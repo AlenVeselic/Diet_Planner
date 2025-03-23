5f01c40b33ef62af6d634d585614cced43997e78
Alen <alen.veselic01@gmail.com>
2025-03-23 21:23:05 +0100

    Make diet plan scrollable

--- Pages/DietPlan.py
+++ Pages/DietPlan.py
@@ -7,25 +7,51 @@ import dietPlanner
 
 class DietPlanPage(Page):
     label = None
+    canvas = None
+    verticalScrollbar = None
+    frame = None
 
     def __init__(self, root, *args, **kwargs):
         Page.__init__(self, root, *args, **kwargs)
-        self.label = Label(self, text="No diet plan generated yet.")
+
+        self.canvas = Canvas(self, borderwidth=0, background="white")
+
+        self.frame = ttk.Frame(self)
+
+        self.verticalScrollBar = Scrollbar(
+            self, orient=VERTICAL, command=self.canvas.yview
+        )
+
+        self.canvas.configure(yscrollcommand=self.verticalScrollBar.set)
+
+        self.verticalScrollBar.pack(side=RIGHT, fill=Y)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas.create_window(
+            (4, 4), window=self.frame, anchor=NW, tags="self.foodFrame"
+        )
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.label = Label(self.frame, text="No diet plan generated yet.")
         self.label.pack(side="top", fill="both", expand=True)
 
-        frame = ttk.Frame(self)
+        buttonframe = ttk.Frame(self.frame)
 
         createDietPlanButton = ttk.Button(
-            frame,
+            buttonframe,
             text="Create Diet Plan",
             command=self.updateLabelWithDietPlan,
         )
 
         createDietPlanButton.pack()
-        frame.pack(side="top", fill="both", expand=True)
+        buttonframe.pack(side="top", fill="both", expand=True)
 
     def updateLabelWithDietPlan(self):
         self.label["text"] = "\n".join(
             "{}\n {}\n".format(k, d)
             for k, d in (dietPlanner.createPlan(5, 2, 2)).items()
         ).replace(",", ",\n")
+
+    def onFrameConfigure(self, event):
+        """Reset the scroll region to encompass the inner frame"""
+        self.canvas.configure(scrollregion=self.canvas.bbox(ALL))
