e911a71ca2d3bbcbbc8210cabfa65e101bcff077
Alen <alen.veselic01@gmail.com>
2025-04-05 18:48:06 +0200

    Make category list scrollable

--- Pages/CategoryList.py
+++ Pages/CategoryList.py
@@ -15,10 +15,30 @@ class CategoryList(Page):
 
     def __init__(self, root, *args, **kwargs):
         Page.__init__(self, root, *args, **kwargs)
-        self.label = Label(self, text="Category List")
+
+        self.canvas = Canvas(self, borderwidth=0, background="white")
+
+        self.pageFrame = ttk.Frame(self.canvas, padding="5")
+        # self.foodFrame.pack(side="top", fill="both", expand=True)
+
+        self.verticalScrollBar = Scrollbar(
+            self, orient=VERTICAL, command=self.canvas.yview
+        )
+
+        self.canvas.configure(yscrollcommand=self.verticalScrollBar.set)
+
+        self.verticalScrollBar.pack(side=RIGHT, fill=Y)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas.create_window(
+            (4, 4), window=self.pageFrame, anchor=NW, tags="self.foodFrame"
+        )
+
+        self.pageFrame.bind("<Configure>", self.onFrameConfigure)
+
+        self.label = Label(self.pageFrame, text="Category List")
         self.label.pack(side="top", fill="both", expand=True)
 
-        self.categoryListFrame = ttk.Frame(self)
+        self.categoryListFrame = ttk.Frame(self.pageFrame)
         self.categoryListFrame.pack(side="top", fill="both", expand=True)
 
         if not self.categories:
@@ -33,9 +53,12 @@ class CategoryList(Page):
                 categoryLabel.pack(side=TOP, expand=True, fill="x")
                 self.categoryWidgets.append(categoryLabel)
 
+    def onFrameConfigure(self, event):
+        """Reset the scroll region to encompass the inner frame"""
+        self.canvas.configure(scrollregion=self.canvas.bbox(ALL))
+
     def refresh(self):
         self.categories = dietPlanner.getCategories()
-        pprint.pprint(self.categories)
 
         for widget in self.categoryWidgets:
             widget.pack_forget()
