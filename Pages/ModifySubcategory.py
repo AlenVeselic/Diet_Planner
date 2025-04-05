8a9b9ae9c604c23158ac241f6f96cb01adcdfc72
Alen <alen.veselic01@gmail.com>
2025-04-05 19:40:10 +0200

    Add scrolling boiler palte to Modify Subcategory

--- Pages/ModifySubcategory.py
+++ Pages/ModifySubcategory.py
@@ -8,7 +8,26 @@ class ModifySubcategory(Page):
 
     def __init__(self, root, *args, **kwargs):
         Page.__init__(self, root, *args, **kwargs)
-        self.label = Label(self, text="WIP: ADD/EDIT Subcategories")
+
+        self.canvas = Canvas(self, borderwidth=0, background="white")
+
+        self.frame = ttk.Frame(self)
+
+        self.verticalScrollBar = Scrollbar(
+            self, orient=VERTICAL, command=self.canvas.yview
+        )
+
+        self.canvas.configure(yscrollcommand=self.verticalScrollBar.set)
+
+        self.verticalScrollBar.pack(side=RIGHT, fill=Y)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas.create_window(
+            (4, 4), window=self.frame, anchor=NW, tags="self.foodFrame"
+        )
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.label = Label(self.frame, text="WIP: ADD/EDIT Subcategories")
         self.label.pack(side="top", fill="both", expand=True)
 
     def refresh(self):
