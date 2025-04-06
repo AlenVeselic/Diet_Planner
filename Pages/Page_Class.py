e55eeaebda8898d589aab0b0afa2867167aa19b0
Alen <alen.veselic01@gmail.com>
2025-04-06 19:31:08 +0200

    Make pages in scrollable canvas expand vertically/centered

--- Pages/Page_Class.py
+++ Pages/Page_Class.py
@@ -7,14 +7,13 @@ class Page(ttk.Frame):
     root: ttk.Window = None
     frame: ttk.Frame = None
 
-    def __init__(self, root, *args, **kwargs):
+    def __init__(self, root: ttk.Window, *args, **kwargs):
         ttk.Frame.__init__(self, *args, **kwargs)
         self.root = root
 
         self.canvas = Canvas(self, borderwidth=0, background="white")
 
         self.frame = ttk.Frame(self.canvas, padding="5")
-        # self.foodFrame.pack(side="top", fill="both", expand=True)
 
         self.verticalScrollBar = Scrollbar(
             self, orient=VERTICAL, command=self.canvas.yview
@@ -23,17 +22,33 @@ class Page(ttk.Frame):
         self.canvas.configure(yscrollcommand=self.verticalScrollBar.set)
 
         self.verticalScrollBar.pack(side=RIGHT, fill=Y)
-        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+
+        windowWidth = self.root.winfo_width() - 50
         self.canvas.create_window(
-            (4, 4), window=self.frame, anchor=NW, tags="self.foodFrame"
+            (4, 4),
+            window=self.frame,
+            anchor=CENTER,
+            tags="self.frame",
         )
 
-        self.frame.bind("<Configure>", self.onFrameConfigure)
+        self.canvas.bind("<Configure>", self.onFrameConfigure)
+        # self.frame.pack(side="top", fill="both", expand=True)
+        self.canvas.pack(side=TOP, fill=BOTH, expand=True)
 
     def onFrameConfigure(self, event):
+        canvas_frame = self.canvas.nametowidget(
+            self.canvas.itemcget("self.frame", "window")
+        )
+        min_width = canvas_frame.winfo_reqwidth()
+        min_height = canvas_frame.winfo_reqheight()
+        if min_width < event.width:
+            self.canvas.itemconfigure("self.frame", width=event.width)
+        if min_height < event.height:
+            self.canvas.itemconfigure("self.frame", height=event.height)
         """Reset the scroll region to encompass the inner frame"""
         self.canvas.configure(scrollregion=self.canvas.bbox(ALL))
 
     def show(self):
+
         self.lift()
         self.root.update()
