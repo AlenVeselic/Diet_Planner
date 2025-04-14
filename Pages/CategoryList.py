8b617c6d2935e895a23587c32f9d45a6ddbd5068
AlenVeselic <alen.veselic01@gmail.com>
2025-04-14 12:01:36 +0200

    Start implementing category frames

--- Pages/CategoryList.py
+++ Pages/CategoryList.py
@@ -29,21 +29,56 @@ class CategoryList(Page):
             noCategoriesLabel.pack(side=TOP, expand=True, fill="x")
             self.categoryWidgets.append(noCategoriesLabel)
         else:
+            categoryHeaderFrame = ttk.Frame(self.categoryListFrame)
+            nameHeaderLabel = Label(categoryHeaderFrame, text="Category Name")
+            nameHeaderLabel.pack(side=LEFT, expand=True)
+            parentHeaderLabel = Label(categoryHeaderFrame, text="Parent Category")
+            parentHeaderLabel.pack(side=LEFT, expand=True)
+            categoryHeaderFrame.pack(side=TOP, expand=True, fill="x")
+            self.categoryWidgets.append(categoryHeaderFrame)
+
             for category in self.categories:
-                categoryLabel = Label(self.categoryListFrame, text=category["name"])
-                categoryLabel.pack(side=TOP, expand=True, fill="x")
-                self.categoryWidgets.append(categoryLabel)
+                categoryFrame = ttk.Frame(self.categoryListFrame)
+                categoryLabel = Label(categoryFrame, text=category["name"])
+                categoryLabel.pack(side=LEFT, expand=True)
+                parentCategory = dietPlanner.getCategoryFromId(self.categories,category["parent_id"])
+                if parentCategory:
+                    categoryParentLabel = Label(
+                        categoryFrame, text=parentCategory["name"]
+                    )
+                else:
+                    categoryParentLabel = Label(categoryFrame, text="None")
+                categoryParentLabel.pack(side=LEFT, expand=True)
+                categoryFrame.pack(side=TOP, expand=True, fill="x")
+                self.categoryWidgets.append(categoryFrame)
 
     def refresh(self):
         self.categories = dietPlanner.getCategories()
 
-        for widget in self.categoryWidgets:
-            widget.pack_forget()
+        for widget in self.categoryListFrame.winfo_children():
+            widget.destroy()
 
         self.categoryWidgets = []
+        categoryHeaderFrame = ttk.Frame(self.categoryListFrame)
+        nameHeaderLabel = Label(categoryHeaderFrame, text="Category Name")
+        nameHeaderLabel.pack(side=LEFT, expand=True)
+        parentHeaderLabel = Label(categoryHeaderFrame, text="Parent Category")
+        parentHeaderLabel.pack(side=LEFT, expand=True)
+        categoryHeaderFrame.pack(side=TOP, expand=True, fill="x")
+
         for category in self.categories:
-            categoryLabel = Label(self.categoryListFrame, text=category["name"])
-            categoryLabel.pack(side=TOP, expand=True, fill="x")
-            self.categoryWidgets.append(categoryLabel)
+            categoryFrame = ttk.Frame(self.categoryListFrame)
+            categoryLabel = Label(categoryFrame, text=category["name"])
+            categoryLabel.pack(side=LEFT, expand=True)
+            parentCategory = dietPlanner.getCategoryFromId(self.categories,category["parent_id"])
+            if parentCategory:
+                categoryParentLabel = Label(
+                    categoryFrame, text=parentCategory["name"]
+                )
+            else:
+                categoryParentLabel = Label(categoryFrame, text="None")
+            categoryParentLabel.pack(side=LEFT, expand=True)
+            categoryFrame.pack(side=TOP, expand=True, fill="x")
+            self.categoryWidgets.append(categoryFrame)
 
         self.root.update()
