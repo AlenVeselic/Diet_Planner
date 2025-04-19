89bce2da098e3878a6f696330762a603bbf2d4f3
Alen <alen.veselic01@gmail.com>
2025-04-19 17:12:59 +0200

    Solve issue with add edit food item page not being responsive

--- Pages/AddEditFoodItem.py
+++ Pages/AddEditFoodItem.py
@@ -248,14 +248,14 @@ class AddEditFoodItem(Page):
 
         self.subcategoryBox.bind("<<ComboboxSelected>>", self.getItems)
 
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(0, weight=1)
 
-        self.grid_rowconfigure(1, weight=2)
-        self.grid_columnconfigure(1, weight=1)
+        self.frame.grid_rowconfigure(1, weight=1)
+        self.frame.grid_columnconfigure(1, weight=1)
 
-        self.grid_rowconfigure(2, weight=1)
-        self.grid_columnconfigure(2, weight=1)
+        self.frame.grid_rowconfigure(2, weight=1)
+        self.frame.grid_columnconfigure(2, weight=1)
 
     def refresh(self):
 
