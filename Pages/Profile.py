bb600256d39b27231eabf7f98a0d0a1220e4bcf1
Alen <alen.veselic01@gmail.com>
2025-04-19 15:51:08 +0200

    Add test labels for profile

--- Pages/Profile.py
+++ Pages/Profile.py
@@ -11,5 +11,10 @@ class Profile(Page):
         self.label = Label(self.frame, text="WIP: User Profile")
         self.label.pack(side="top", fill="both", expand=True)
 
+        self.weightLabel = Label(self.frame, text="Weight")
+        self.weightLabel.pack(side=LEFT, fill="x", expand=True)
+        self.weightValueLabel = Label(self.frame, text="9000 KG")
+        self.weightValueLabel.pack(side=RIGHT, fill="x", expand=True)
+
     def refresh(self):
         self.root.update()
